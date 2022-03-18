b73b57792711ad60368c9dffe38be5cbadf8ed4a
yueshuiqizi <101640896+yueshuiqizi@users.noreply.github.com>
2022-03-18 09:05:55 +0800

    pull commit

--- mybatis/main/Git/src/main/java/git/GitTest.java
+++ mybatis/main/Git/src/main/java/git/GitTest.java
@@ -7,8 +7,8 @@ public class GitTest {
         System.out.println("hello3");
         System.out.println("master test");
         System.out.println("huixi test");
-        System.out.println("push test");
-        System.out.println("pull test");
+        System.out.println("push test1");
+        System.out.println("pull test1");
         
         
 
